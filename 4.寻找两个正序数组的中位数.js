208a679c0fa6f278cbde3f5562cb8bc7b0ca0593
wuliang <937743837@qq.com>
2023-07-28 00:17:36 +0800

    新增4的备注

--- 4.寻找两个正序数组的中位数.js
+++ 4.寻找两个正序数组的中位数.js
@@ -11,16 +11,21 @@
  * @return {number}
  */
 var findMedianSortedArrays = function (nums1, nums2) {
+  // 合并数组
   let num = nums1.concat(nums2)
 
+  // 排序数组
   let sortNum = num.sort((a, b) => {
     return a - b
   })
   let length = sortNum.length
   let finalResult = -1;
+  // 长度是否为偶数
   if (!(length % 2)) {
+    // 偶数取中间两位除以2
     finalResult = (sortNum[length / 2 - 1] + sortNum[length / 2]) / 2
   } else {
+    // 奇数直接取中间的
     finalResult = sortNum[Math.floor(length / 2)]
   }
   return finalResult;
@@ -28,6 +33,6 @@ var findMedianSortedArrays = function (nums1, nums2) {
 };
 // @lc code=end
 
-let num1 = [1, 2]
-let num2 = [3, 4]
-findMedianSortedArrays(num1, num2)
+// let num1 = [1, 2]
+// let num2 = [3, 4]
+// findMedianSortedArrays(num1, num2)
